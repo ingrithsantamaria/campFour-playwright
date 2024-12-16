ecf71dfcc6794cd1ca8647b6f148d186979783f6
ingrithsantamaria <ingrithsanta@gmail.com>
2024-12-16 16:23:30 -0500

    update README file

--- playwright/e2e/tests/register/registerTest.spec.js
+++ playwright/e2e/tests/register/registerTest.spec.js
@@ -1,8 +1,8 @@
 import { test } from "@playwright/test";
 import { RegisterUser } from "../../pages/register/registerPage";
 import { Home } from "../../pages/home/home";
-test.describe("User registration", () => {
-  test("Create a user with valid data [@smoke]", async ({ page }) => {
+test.describe("User registration",{tag: '@smoke'}, () => {
+  test("Create a user with valid data", async ({ page }) => {
     const registerPage = new RegisterUser(page);
     const home = new Home(page);
     await registerPage.navigate();
