e3af73d74ae72ff14455d14323e28df3aafca329
ingrithsantamaria <ingrithsanta@gmail.com>
2024-12-16 16:04:10 -0500

    creating user on mobile

--- playwright/e2e/pages/register/registerPage.js
+++ playwright/e2e/pages/register/registerPage.js
@@ -2,23 +2,34 @@ import { faker } from "@faker-js/faker";
 export class RegisterUser {
   constructor(page) {
     this.page = page;
-    this.userIcon =
-      'button[data-action="click->slideover-account#toggle click@window->slideover-account#hide click->toggle-menu#hide touch->toggle-menu#hide"]';
+    this.hamburgerMenu = 'button[data-toggle-menu-target="button"]'
+    this.iconUserMobile = 'button#mobile-menu'
+    this.iconUser = 'button[data-action="click->slideover-account#toggle click@window->slideover-account#hide click->toggle-menu#hide touch->toggle-menu#hide"]'
     this.emailInput = "input#user_email";
     this.passwordInput = "input#user_password";
     this.repeatPassword = "input#user_password_confirmation";
     this.signUpButton = 'div a[href="/user/sign_up"]';
     this.submit = 'input[type="submit"]';
   }
+
   async navigate() {
     await this.page.goto("https://demo.spreecommerce.org");
   }
-  async selectUserIcon() {
-    await this.page.click(this.userIcon);
+
+  async openLogin() {
+    const isMobile = this.page.viewportSize().width < 768
+    if(isMobile) {
+      await this.page.click(this.hamburgerMenu)
+      await this.page.click(this.iconUserMobile)
+    }else{
+      await this.page.click(this.iconUser);
+    }
   }
+
   async selectSignUp() {
     await this.page.click(this.signUpButton, { waitUntil: "domcontentloaded", timeout: 2000 });
   }
+
   async fillForm() {
     const password = faker.internet.password(10, true);
     const email = faker.internet.email();
@@ -26,6 +37,7 @@ export class RegisterUser {
     await this.page.fill(this.passwordInput, password);
     await this.page.fill(this.repeatPassword, password);
   }
+
   async signUp() {
     await this.page.click(this.submit);
   }
--- playwright/e2e/tests/register/registerTest.spec.js
+++ playwright/e2e/tests/register/registerTest.spec.js
@@ -1,15 +1,15 @@
-import { test} from "@playwright/test"
-import { RegisterUser } from "../../pages/register/registerPage"
+import { test } from "@playwright/test";
+import { RegisterUser } from "../../pages/register/registerPage";
 import { Home } from "../../pages/home/home";
 test.describe("User registration", () => {
   test("Create a user with valid data [@smoke]", async ({ page }) => {
     const registerPage = new RegisterUser(page);
-    const home = new Home(page)
-    await registerPage.navigate()
-    await registerPage.selectUserIcon()
-    await registerPage.selectSignUp()
-    await registerPage.fillForm()
-    await registerPage.signUp()
-    await home.isOnHomePage()
+    const home = new Home(page);
+    await registerPage.navigate();
+    await registerPage.openLogin();
+    await registerPage.selectSignUp();
+    await registerPage.fillForm();
+    await registerPage.signUp();
+    await home.isOnHomePage();
   });
 });
