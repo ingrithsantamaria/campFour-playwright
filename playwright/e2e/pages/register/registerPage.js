87042647d65d3b6f4461c09e28d3c18c84faed5a
ingrithsantamaria <ingrithsanta@gmail.com>
2025-01-07 12:58:21 -0500

    refactor: file creation for selectors

--- playwright/e2e/pages/register/registerPage.js
+++ playwright/e2e/pages/register/registerPage.js
@@ -1,15 +1,8 @@
 import { faker } from "@faker-js/faker";
+import { selectors } from "../../selectors/registerSelectors";
 export class RegisterUser {
   constructor(page) {
     this.page = page;
-    this.hamburgerMenu = 'button[data-toggle-menu-target="button"]'
-    this.iconUserMobile = 'button#mobile-menu'
-    this.iconUser = 'button[data-action="click->slideover-account#toggle click@window->slideover-account#hide click->toggle-menu#hide touch->toggle-menu#hide"]'
-    this.emailInput = "input#user_email";
-    this.passwordInput = "input#user_password";
-    this.repeatPassword = "input#user_password_confirmation";
-    this.signUpButton = 'div a[href="/user/sign_up"]';
-    this.submit = 'input[type="submit"]';
   }
 
   async navigate() {
@@ -19,27 +12,27 @@ export class RegisterUser {
   async openLogin() {
     const isMobile = this.page.viewportSize().width < 768
     if(isMobile) {
-      await this.page.click(this.hamburgerMenu)
-      await this.page.click(this.iconUserMobile)
+      await this.page.click(selectors.hamburgerMenu)
+      await this.page.click(selectors.iconUserMobile)
     }else{
-      await this.page.click(this.iconUser);
+      await this.page.click(selectors.iconUser);
     }
   }
 
   async selectSignUp() {
-    await this.page.click(this.signUpButton);
-    await this.page.waitForSelector(this.repeatPassword)
+    await this.page.click(selectors.signUpButton);
+    await this.page.waitForSelector(selectors.repeatPassword)
   }
 
   async fillForm() {
     const password = faker.internet.password(10, true);
     const email = faker.internet.email();
-    await this.page.fill(this.emailInput, email);
-    await this.page.fill(this.passwordInput, password);
-    await this.page.fill(this.repeatPassword, password);
+    await this.page.fill(selectors.emailInput, email);
+    await this.page.fill(selectors.passwordInput, password);
+    await this.page.fill(selectors.repeatPassword, password);
   }
 
   async signUp() {
-    await this.page.click(this.submit);
+    await this.page.click(selectors.submit);
   }
 }
