511a13aa440434d4d1c4410dd41dc37812c547c3
ingrithsantamaria <ingrithsanta@gmail.com>
2025-01-14 09:07:37 -0500

    refactoring:call the method only once

--- playwright/e2e/tests/register/registerTest.spec.js
+++ playwright/e2e/tests/register/registerTest.spec.js
@@ -6,36 +6,29 @@ test.describe("User registration", () => {
   test.beforeEach(async ({ page }) => {
     registerPage = new RegisterUser(page);
     await registerPage.navigate();
+    await registerPage.openLogin();
+    await registerPage.selectSignUp();
   });
   test("Create a user with valid data", { tag: "@smoke" }, async ({ page }) => {
     const home = new Home(page);
-    await registerPage.openLogin();
-    await registerPage.selectSignUp();
     await registerPage.fillForm();
     await registerPage.signUp();
     await home.isOnHomePage();
   });
 
   test("Create a user with existing email", async ({ page }) => {
-    await registerPage.openLogin();
-    await registerPage.selectSignUp();
     await registerPage.fillFormWithExistingEmail();
     await registerPage.signUp();
     await registerPage.messageError();
   });
 
   test("Create a user with invalid email", async ({ page }) => {
-    await registerPage.openLogin();
-    await registerPage.selectSignUp();
     await registerPage.fillFormInvalidEmail();
     await registerPage.signUp();
     await registerPage.mailInvalid();
   });
 
   test("Create a user with missing data", async ({ page }) => {
-    await registerPage.navigate();
-    await registerPage.openLogin();
-    await registerPage.selectSignUp();
     await registerPage.fillFormWithMissingData();
     await registerPage.signUp();
     await registerPage.messageError();
